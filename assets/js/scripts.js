317865c971e987e6d839245417f93c086587e8c3
thetoothless <ninaling@ucla.edu>
2017-03-31 15:32:24 -0700

    basic interactive animations on splash of index page, reduced image size for members page

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,4 +1,5 @@
 (function($) {
+
     "use strict";
 
     $('body').scrollspy({
@@ -25,6 +26,16 @@
           scrollTop: $("#rush-main").offset().top - 60
       }, 1450, 'easeInOutExpo');
       event.preventDefault();
+    });
+
+    $(document).mousemove(function(event){
+      if ($("#shards-container")) {
+        var location = 0.5 * $(document).width() - event.pageX;
+        $("#shards-img-above").css("transform", "rotateY(" + (Math.floor(location / 150) * 30) * (-2) + "deg)");
+        $("#shards-img-mid").css("transform", "rotateY(" + Math.floor(location / 300) * 300 + "deg)");
+        // $("#shards-img-below").css("transform", "rotateY(" + Math.floor((0.5 * $(document).width() - event.pageX) / 200) * 50 + "deg)");
+        $("#shards-img-large").css("transform", "rotateY(" + Math.floor(location / 200) * 5 + "deg) scale(" + (1 + Math.floor(Math.abs(location) / 200) / 20) + ")");
+      }
     });
 
 })(jQuery);
