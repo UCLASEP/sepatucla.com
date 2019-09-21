66cd679f1c72c7a56818dfeb403ea4419a6ebd81
Alissa Niewiadomski <aniewiadomski@ucla.edu>
2019-09-20 20:22:16 -0700

    testing

--- scripts/install-pre-commit-hook.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-gitroot=$(git rev-parse --show-toplevel || echo ".")
-cp $gitroot/scripts/pre-commit.sample $gitroot/.git/hooks/pre-commit
-chmod u+x $gitroot/.git/hooks/pre-commit
-
-success_message="\n\n✅ Pre-commit hook installed. Happy developing! 🎉 🛁 🎉 🛁 🎉\n"
-
-echo -e $success_message
