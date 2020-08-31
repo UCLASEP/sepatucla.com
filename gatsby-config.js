307eef0559889a5777b2d776dce439440d86434f
Alissa Niewiadomski <aniewiadomski@ucla.edu>
2020-08-31 00:44:13 -0700

    careers page basic layout with new logos

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,12 +1,20 @@
 module.exports = {
   plugins: [
+    `gatsby-plugin-sharp`,
+    `gatsby-transformer-sharp`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/assets/images/`,
+      },
+    },
     'gatsby-plugin-styled-components',
     {
       resolve: `gatsby-plugin-prefetch-google-fonts`,
       options: {
         fonts: [
           {
-            family: `Fira Sans`,
+            family: `Fira Sans:400,600`,
           },
         ],
       },
---
+++ src/constants/careers/medicine.js
@@ -0,0 +1,7 @@
+const HELIX = {
+  alt: 'Helix logo',
+  src: 'helix',
+  link: 'https://www.helix.com/',
+};
+
+export default [HELIX];
