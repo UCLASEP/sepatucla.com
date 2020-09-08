4ec95f496af82dc459ff6469c5c3a111f26f95c4
harik2000 <harikrishna.s2000@gmail.com>
2020-09-07 17:37:37 -0700

    Rush dates (#19)
    * homepage
    
    * homepage
    
    * rush update

--- src/styles/global.js
+++ src/styles/global.js
@@ -15,6 +15,7 @@ export const GREY40 = 'rgba(34, 34, 34, 0.7)';
  */
 export const HEADER_FONT_SIZES = {
   l: '52px',
+  ml: '48px',
   m: '32px',
   s: '24px',
   xs: '14px',
@@ -31,6 +32,7 @@ export const TEXT_FONT_SIZES = {
  * Spacing
  */
 export const MARGINS = {
+  xl: '256px',
   l: '128px',
   m: '68px',
   s: '16px',
