257e939d5fab452803a7c46480cc0dec52507e45
Alissa Niewiadomski <aniewiadomski@ucla.edu>
2020-09-12 22:24:15 -0700

    careers page basic layout with new logos (#25)

---
+++ src/constants/careers/tech.js
@@ -0,0 +1,164 @@
+// Tech
+const AIRBNB = {
+  alt: 'Airbnb logo',
+  src: 'airbnb',
+  link: 'https://news.airbnb.com/',
+};
+
+const APPLE = {
+  alt: 'Apple logo',
+  src: 'apple',
+  link: 'https://www.apple.com/',
+};
+
+const AMAZON = {
+  alt: 'Amazon logo',
+  src: 'amazon',
+  link: 'https://www.aboutamazon.com/our-company',
+};
+
+const COURSERA = {
+  alt: 'Coursera logo',
+  src: 'coursera',
+  link: 'https://www.coursera.org/',
+};
+
+const DROPBOX = {
+  alt: 'Dropbox logo',
+  src: 'dropbox',
+  link: 'https://www.dropbox.com/about',
+};
+
+const FACEBOOK = {
+  alt: 'Facebook logo',
+  src: 'facebook',
+  link: 'https://about.fb.com/company-info/',
+};
+
+const GOOGLE = {
+  alt: 'Google logo',
+  src: 'google',
+  link: 'https://about.google/',
+};
+
+const INTUIT = {
+  alt: 'Intuit logo',
+  src: 'intuit',
+  link: 'https://www.intuit.com/company/',
+};
+
+const LINKEDIN = {
+  alt: 'LinkedIn logo',
+  src: 'linkedin',
+  link: 'https://about.linkedin.com/',
+};
+
+const LYFT = {
+  alt: 'Lyft logo',
+  src: 'lyft',
+  link: 'https://www.lyft.com/careers',
+};
+
+const MEDIUM = {
+  alt: 'Medium logo',
+  src: 'medium',
+  link: 'https://medium.com/jobs-at-medium/work-at-medium-959d1a85284e',
+};
+
+const MICROSOFT = {
+  alt: 'Microsoft logo',
+  src: 'microsoft',
+  link: 'https://careers.microsoft.com/us/en',
+};
+
+const PAYPAL = {
+  alt: 'PayPal logo',
+  src: 'paypal',
+  link: 'https://www.paypal.com/us/webapps/mpp/about',
+};
+
+const REDDIT = {
+  alt: 'Reddit logo',
+  src: 'reddit',
+  link: 'https://redditinc.com/',
+};
+
+const ROBINHOOD = {
+  alt: 'Robinhood logo',
+  src: 'robinhood',
+  link: 'https://robinhood.com/',
+};
+
+const SLACK = {
+  alt: 'Slack logo',
+  src: 'slack',
+  link: 'https://slack.com/about',
+};
+
+const SPACEX = {
+  alt: 'SpaceX logo',
+  src: 'spacex',
+  link: 'https://www.spacex.com/',
+};
+
+const STRIPE = {
+  alt: 'Stripe logo',
+  src: 'stripe',
+  link: 'https://stripe.com/',
+};
+
+const TECHCRUNCH = {
+  alt: 'TechCrunch logo',
+  src: 'techcrunch',
+  link: 'https://techcrunch.com/',
+};
+
+const TECHSTARS = {
+  alt: 'Techstars logo',
+  src: 'techstars',
+  link: 'https://www.techstars.com/',
+};
+
+const TESLA = {
+  alt: 'Tesla logo',
+  src: 'tesla',
+  link: 'https://www.tesla.com/',
+};
+
+const TINDER = {
+  alt: 'Tinder logo',
+  src: 'tinder',
+  link: 'https://www.gotinder.com/jobs?lang=en',
+};
+
+const UBER = {
+  alt: 'Uber logo',
+  src: 'uber',
+  link: 'https://www.uber.com/us/en/about/',
+};
+
+export default [
+  GOOGLE,
+  AIRBNB,
+  AMAZON,
+  MEDIUM,
+  REDDIT,
+  FACEBOOK,
+  TECHCRUNCH,
+  APPLE,
+  TESLA,
+  SPACEX,
+  ROBINHOOD,
+  LYFT,
+  UBER,
+  COURSERA,
+  TINDER,
+  SLACK,
+  MICROSOFT,
+  DROPBOX,
+  LINKEDIN,
+  PAYPAL,
+  STRIPE,
+  TECHSTARS,
+  INTUIT,
+];
--- src/pages/careers.jsx
+++ src/pages/careers.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 
 import PageLayout from '../components/layouts/PageLayout';
 import PageHero from '../components/layouts/PageHero';
+import CareersContent from '../components/layouts/careers/CareersContent';
 
 const CareersPage = () => (
   <PageLayout location="careers">
@@ -12,6 +13,7 @@ const CareersPage = () => (
       blurb="An intrapreneur strives to create a culture innovative product development within an organization.<br/><br/>Our actives and alumni have done meaningful and impactful work at various companies across the world."
       heroImg="members/members-hero"
     />
+    <CareersContent />
   </PageLayout>
 );
 
