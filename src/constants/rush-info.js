457bfab8f2deefea0c7482530b8cb4a613b930c6
harik2000 <harikrishna.s2000@gmail.com>
2020-10-01 22:01:50 -0700

    rush update (#35)

--- src/constants/rush-info.js
+++ src/constants/rush-info.js
@@ -1,27 +1,27 @@
 const DAY_ONE = {
   title: 'Meet the Chapter',
-  date: 'October 1',
-  location: 'Bruin Viewpoint Room, 7-9pm',
+  date: 'October 5',
+  location: 'Virtual, 7-9pm',
   dress: 'Casual Attire',
 };
 
 const DAY_TWO = {
   title: 'Social Night',
-  date: 'October 2',
-  location: 'LATC Straus Clubhouse, 7-9pm',
+  date: 'October 6',
+  location: 'Virtual, 7-9pm',
   dress: 'Casual Attire',
 };
 
 const DAY_THREE = {
   title: 'Professional Night',
-  date: 'October 3',
-  location: 'Pauley Pavilion m10a, 7-9pm',
+  date: 'October 7',
+  location: 'Virtual, 7-9pm',
   dress: 'Business Casual Attire',
 };
 
 const DAY_FOUR = {
   title: 'Coffee Chat',
-  date: 'October 4',
+  date: 'October 8',
   location: 'Invite Only, TBD',
   dress: 'Casual Attire',
 };
