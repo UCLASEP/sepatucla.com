ddbda22fbdb152fcd5435868c48fb3f50720b580
Alissa Niewiadomski <aniewiadomski@ucla.edu>
2020-10-04 00:14:16 -0700

    bug fixes

--- .cache/async-requires.js
+++ .cache/async-requires.js
@@ -2,11 +2,6 @@
 const preferDefault = m => m && m.default || m
 
 exports.components = {
-  "component---src-pages-careers-jsx": () => import("./../../src/pages/careers.jsx" /* webpackChunkName: "component---src-pages-careers-jsx" */),
-  "component---src-pages-exec-jsx": () => import("./../../src/pages/exec.jsx" /* webpackChunkName: "component---src-pages-exec-jsx" */),
-  "component---src-pages-index-jsx": () => import("./../../src/pages/index.jsx" /* webpackChunkName: "component---src-pages-index-jsx" */),
-  "component---src-pages-members-jsx": () => import("./../../src/pages/members.jsx" /* webpackChunkName: "component---src-pages-members-jsx" */),
-  "component---src-pages-recruitment-jsx": () => import("./../../src/pages/recruitment.jsx" /* webpackChunkName: "component---src-pages-recruitment-jsx" */),
-  "component---src-pages-ventures-jsx": () => import("./../../src/pages/ventures.jsx" /* webpackChunkName: "component---src-pages-ventures-jsx" */)
+  "component---cache-dev-404-page-js": () => import("./../../.cache/dev-404-page.js" /* webpackChunkName: "component---cache-dev-404-page-js" */)
 }
 
--- .cache/sync-requires.js
+++ .cache/sync-requires.js
@@ -5,11 +5,6 @@ const preferDefault = m => m && m.default || m
 
 
 exports.components = {
-  "component---src-pages-careers-jsx": hot(preferDefault(require("/Users/aniewiadomski/projects/uclasep.github.io/src/pages/careers.jsx"))),
-  "component---src-pages-exec-jsx": hot(preferDefault(require("/Users/aniewiadomski/projects/uclasep.github.io/src/pages/exec.jsx"))),
-  "component---src-pages-index-jsx": hot(preferDefault(require("/Users/aniewiadomski/projects/uclasep.github.io/src/pages/index.jsx"))),
-  "component---src-pages-members-jsx": hot(preferDefault(require("/Users/aniewiadomski/projects/uclasep.github.io/src/pages/members.jsx"))),
-  "component---src-pages-recruitment-jsx": hot(preferDefault(require("/Users/aniewiadomski/projects/uclasep.github.io/src/pages/recruitment.jsx"))),
-  "component---src-pages-ventures-jsx": hot(preferDefault(require("/Users/aniewiadomski/projects/uclasep.github.io/src/pages/ventures.jsx")))
+  "component---cache-dev-404-page-js": hot(preferDefault(require("/Users/aniewiadomski/projects/uclasep.github.io/.cache/dev-404-page.js")))
 }
 
