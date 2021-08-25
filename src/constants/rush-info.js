ec43c10637efd929c444553720b7a520c09efb08
harik2000 <harikrishna.s2000@gmail.com>
2021-08-25 12:48:48 -0700

    Hari test (#58)
    * test
    
    * recruitment initial update

--- src/constants/rush-info.js
+++ src/constants/rush-info.js
@@ -1,27 +1,27 @@
 const DAY_ONE = {
   title: 'Meet the Chapter',
-  date: 'March 29',
-  location: 'Virtual, 7-9pm',
+  date: 'September 27',
+  location: 'Location TBD, 7-9pm',
   dress: 'Casual Attire',
 };
 
 const DAY_TWO = {
   title: 'Social Night',
-  date: 'March 30',
-  location: 'Virtual, 7-9pm',
+  date: 'September 28',
+  location: 'Location TBD, 7-9pm',
   dress: 'Casual Attire',
 };
 
 const DAY_THREE = {
   title: 'Professional Night',
-  date: 'March 31',
-  location: 'Virtual, 7-9pm',
+  date: 'September 29',
+  location: 'Location TBD, 7-9pm',
   dress: 'Business Casual Attire',
 };
 
 const DAY_FOUR = {
   title: 'Coffee Chat',
-  date: 'April 1',
+  date: 'September 30',
   location: 'Invite Only, TBD',
   dress: 'Casual Attire',
 };
